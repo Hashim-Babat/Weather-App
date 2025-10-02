8a45cfa2c2b5409e8bdeec7016fa5e5593a1607d
Hashim-Babat <babathashim@gmail.com>
2025-10-02 21:30:48 +0530

    feature added

--- frontend/src/Components/WeatherCard.jsx
+++ frontend/src/Components/WeatherCard.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
 function WeatherCard({ data }) {
+  if (!data) return null;
+
+  const iconCode = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
   return (
-    <div className="text-center mb-6">
-      <h2 className="text-lg font-semibold">{data.name}</h2>
-      <p className="text-5xl font-bold">{Math.round(data.main.temp)}°C</p>
-      <p className="capitalize">{data.weather[0].description}</p>
-      <p>Humidity: {data.main.humidity}%</p>
+    <div className="flex items-center justify-between mb-6 p-4 rounded-xl ">
+      
+      <div className="flex-shrink-0">
+        <img src={iconUrl} alt={data.weather[0].description} className="w-44 h-44" />
+      </div>
+      
+      <div className="text-left flex-1 ml-14">
+        <h2 className="text-xl font-semibold">{data.name}</h2>
+        <p className="text-5xl font-bold">{Math.round(data.main.temp)}°C</p>
+        <p className="capitalize text-lg">{data.weather[0].description}</p>
+        <p className="text-md">Humidity: {data.main.humidity}%</p>
+      </div>
     </div>
   );
 }
